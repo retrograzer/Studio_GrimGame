83289d9441cded09a6d7921f88728f344a228c59
Noah Schultz <noahgschultz@gmail.com>
2024-02-25 16:09:09 -0500

    Small changes to work with resolution
    -Fixed target resolution
    -changed Icon script to work modularly

--- Assets/_Scripts/IconScript.cs
+++ Assets/_Scripts/IconScript.cs
@@ -5,25 +5,30 @@ using UnityEngine;
 public class IconScript : MonoBehaviour
 {
     public GameObject icon;
+    public Transform[] buttonPositions;
 
     // Start is called before the first frame update
     void Start()
     {
-        icon.transform.position = new Vector3(330, 240, 0);
+        //icon.transform.position = new Vector3(330, 240, 0);
+        icon.transform.position = buttonPositions[0].position;
     }
 
     public void button1Pos()
     {
-        icon.transform.position = new Vector3(330, 240, 0);
+        //icon.transform.position = new Vector3(330, 240, 0);
+        icon.transform.position = buttonPositions[0].position;
     }
 
     public void button2Pos()
     {
-        icon.transform.position = new Vector3(330, 160, 0);
+        //icon.transform.position = new Vector3(330, 160, 0);
+        icon.transform.position = buttonPositions[1].position;
     }
 
     public void button3Pos()
     {
-        icon.transform.position = new Vector3(330, 90, 0);
+        //icon.transform.position = new Vector3(330, 90, 0);
+        icon.transform.position = buttonPositions[2].position;
     }
 }
