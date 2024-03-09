c1fbaafd32958c571b34340680da2b8681593105
ifranAm <ifranambahun@gmail.com>
2024-03-08 20:19:44 -0500

    Buttons

--- Assets/_Scripts/IconScript.cs
+++ Assets/_Scripts/IconScript.cs
@@ -5,25 +5,28 @@ using UnityEngine;
 public class IconScript : MonoBehaviour
 {
     public GameObject icon;
+    public GameObject empty1;
+    public GameObject empty2;
+    public GameObject empty3;
 
     // Start is called before the first frame update
     void Start()
     {
-        icon.transform.position = new Vector3(330, 240, 0);
+        icon.transform.position = empty1.transform.position;
     }
 
     public void button1Pos()
     {
-        icon.transform.position = new Vector3(330, 240, 0);
+        icon.transform.position = empty1.transform.position;
     }
 
     public void button2Pos()
     {
-        icon.transform.position = new Vector3(330, 160, 0);
+        icon.transform.position = empty2.transform.position;
     }
 
     public void button3Pos()
     {
-        icon.transform.position = new Vector3(330, 90, 0);
+        icon.transform.position = empty3.transform.position;
     }
 }
