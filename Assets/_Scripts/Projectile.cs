0ccdb628ed28404caee44657ef20cddb8569a26f
Noah Schultz <noahgschultz@gmail.com>
2024-03-16 16:35:32 -0400

    Edited enemies to accept knockback bs

--- Assets/_Scripts/Projectile.cs
+++ Assets/_Scripts/Projectile.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public int damage = 1; // change damage
+    public bool friendlyFire = true;
+
+    Collider2D coll;
+
+    private void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+        coll.enabled = false;
+    }
+
+    private void Start()
+    {
+        Destroy(gameObject, 5);
+        Invoke("EnableCollision", 1f);
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -15,5 +30,17 @@ public class Projectile : MonoBehaviour
 
             Destroy(gameObject);
         }
+
+        if (collision.CompareTag("Enemy") && friendlyFire)
+        {
+            collision.GetComponent<EnemyHealth>().TakeDamage(damage, transform.position);
+
+            Destroy(gameObject);
+        }
+    }
+
+    void EnableCollision ()
+    {
+        coll.enabled = true;
     }
 }
