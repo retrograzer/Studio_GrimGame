aa5ddbbe4c56c8d41bc3a314ba6a42a19ebfdc76
Noah Schultz <noahgschultz@gmail.com>
2024-04-18 19:22:42 -0400

    Dropping Souls on death

--- Assets/_Scripts/PlayerInteraction.cs
+++ Assets/_Scripts/PlayerInteraction.cs
@@ -13,6 +13,7 @@ public class PlayerInteraction : MonoBehaviour
     private void Awake()
     {
         pm = GetComponent<PlayerManager>();
+        SoulNumChange(0);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -36,15 +37,43 @@ public class PlayerInteraction : MonoBehaviour
     public void PickupSoul (int soulsGained)
     {
         soulsHeld += soulsGained;
-        pm.pui.soulsHeldText.text = "Souls: " + soulsHeld;
+        pm.pui.soulsHeldText.text = "" + soulsHeld;
         fx.Play();
         pm.pac.pickupSFX.clip = pm.pac.rngPickupClip[Random.Range(0, pm.pac.rngPickupClip.Length)];
         pm.pac.pickupSFX.Play();
+        SoulNumChange(soulsGained);
+    }
+
+    public void SoulNumChange(int change)
+    {
+        //if (numOfSouls + change >= 0)
+        //{
+        //    numOfSouls += change;
+        //}
+        //else
+        //{
+        //    numOfSouls = 0;
+        //}
+
+        int newRed = 255 - (soulsHeld * 10);
+        if (newRed < 0)
+        {
+            newRed = 0;
+        }
+        pm.pui.soulOrbImg.color = new Color32((byte)newRed, 255, 255, 255);
+
+        int newTextColorNum = soulsHeld * 20;
+        if (newTextColorNum > 255)
+        {
+            newTextColorNum = 255;
+        }
+        pm.pui.soulsHeldText.color = new Color32((byte)newTextColorNum, (byte)newTextColorNum, (byte)newTextColorNum, 255);
     }
 
     public void DepositSoulsFX (int soulsRemoved)
     {
         soulsHeld -= soulsRemoved;
+        pm.pui.soulsHeldText.text = "" + soulsHeld;
     }
 
     public void ApplySlowEffect ()
--- Assets/_Scripts/PlayerUI.cs
+++ Assets/_Scripts/PlayerUI.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour
 {
     public TextMeshProUGUI healthText;
     public TextMeshProUGUI soulsHeldText;
     public GameObject gameOverCanvas;
+    public Image soulOrbImg;
+    public GameObject[] healthIcons;
+    //public GameObject[] lifeIcons;
 }
